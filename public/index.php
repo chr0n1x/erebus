4fca227f659648b4647e555940dde2e31613b659
chr0n1x <heilong24@gmail.com>
2014-01-04 01:50:47 -0500

    public/index: wrong const names

--- public/index.php
+++ public/index.php
@@ -6,13 +6,13 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => Erebus_VIEWS_DIR
+    'twig.path' => EREBUS_VIEWS_DIR
 ));
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => Erebus_LOGS_DIR . '/all.log'
+    'monolog.logfile' => EREBUS_LOGS_DIR . '/all.log'
 ));
 
-$configs = glob( Erebus_CONFIG_DIR . '/*.json' );
+$configs = glob( EREBUS_CONFIG_DIR . '/*.json' );
 foreach ( $configs as $file ) {
   $app->register( new Igorw\Silex\ConfigServiceProvider( $file ) );
 }
